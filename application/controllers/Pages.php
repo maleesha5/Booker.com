eaf84dd04a17683ac51ab71a60c4997878a21c0f
unknown <maleesha55@gmail.com>
2018-10-12 02:15:19 +0530

    first commit

---
+++ application/controllers/Pages.php
@@ -0,0 +1,17 @@
+<?php
+class Pages extends CI_Controller
+{
+    public function view($page = 'home')
+    {
+        if(!file_exists(APPPATH.'views/'.$page.'.php')){
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page);
+
+        $this->load->view('header');
+        $this->load->view($page);
+        $this->load->view('footer');
+
+    }
+}
